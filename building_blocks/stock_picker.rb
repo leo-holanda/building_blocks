172e17975d873e0a22fb97aa0f85efb3de257dfc
leo-holanda <leoholaanda2@gmail.com>
2019-05-30 16:20:06 -0300

    stock picker done

---
+++ building_blocks/stock_picker.rb
@@ -0,0 +1,23 @@
+def stock_picker(stock_prices)
+    buy_index = nil
+    sell_index = nil
+    profit = stock_prices[1] - stock_prices[2]
+
+    stock_prices.each_with_index { |price_1, index_1|
+        stock_prices[(index_1+1)..-1].each_with_index { |price_2, index_2|
+            
+            #puts("#{price_1}[#{index_1}] - #{price_2}[#{index_2 + index_1 + 1}] = #{price_2 - price_1}")
+            
+            if price_2 - price_1 > profit
+                buy_index = index_1
+                sell_index = index_2 + index_1 + 1
+                profit = price_2 - price_1
+            end
+        }
+    }
+        
+    #puts("#{stock_prices[buy_index]} - #{stock_prices[sell_index]} = #{profit}")
+    puts("[#{buy_index},#{sell_index}]")
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
