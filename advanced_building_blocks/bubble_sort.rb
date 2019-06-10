86c7d17b02754715609fa1070c9443f84a8da4d9
leo-holanda <leoholaanda2@gmail.com>
2019-06-10 17:42:11 -0300

    initial bubble sort done

--- advanced_building_blocks/bubble_sort.rb
+++ advanced_building_blocks/bubble_sort.rb
@@ -1,3 +1,10 @@
+class Array
+    def swap!(a,b)
+        self[a], self[b] = self[b], self[a]
+    self
+    end
+end
+
 def get_numbers()
     numbers = []
     puts "Enter n to end"
@@ -20,9 +27,30 @@ def get_numbers()
     return numbers
 end
 
-def bubble_sort()
+def bubble_sort(numberArray)
+    is_sorted = false
     
+    until is_sorted do
+        is_sorted = true
+        numberArray.each_with_index { |number, index|
+            if index == numberArray.length - 1
+                next
+            elsif
+                next_number = numberArray[index + 1]
+                if number > next_number
+                    is_sorted = false
+                    numberArray.swap!(index, index + 1)
+                end
+            end
+        }
+    end
 end
 
 numbers_input = get_numbers
+puts "Number's list"
+print numbers_input 
+puts ""
 bubble_sort(numbers_input)
+puts "Sorted number's list"
+print numbers_input 
+puts ""
